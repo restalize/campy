2ac09b30d5a45e6b7effded41e825fd9b913ed18
restalize <vila.remedi@gmail.com>
2017-05-22 22:26:32 +0300

    icons

--- www.campeonbet.com/skins/campeonbet.com/js/customtext.js
+++
@@ -1,5 +0,0 @@
-  $('.ng-scope').each(function(i){
-        var color = $(this).css('color');
-        if (color == 'rgba(255,255,255,.73)' || color == 'white' && background-color == 'ffb703') //=='blue' <- IE hack
-            $ng-scope.customStyle.colorClass = "black";
-    });
